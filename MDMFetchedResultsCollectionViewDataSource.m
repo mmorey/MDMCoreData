a1da39b2f1accf2b29ecea5ad0ba1aaa78a59100
Terry Lewis <terrylewis@outlook.com>
2014-03-17 19:45:27 -0500

    Added collectionView data source classes.

---
+++ MDMFetchedResultsCollectionViewDataSource.m
@@ -0,0 +1,263 @@
+//
+// Created by Terry Lewis II on 3/3/14.
+// Copyright (c) 2014 Chai One. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+#import "MDMFetchedResultsCollectionViewDataSource.h"
+#import "MDMCoreDataMacros.h"
+
+@interface MDMFetchedResultsCollectionViewDataSource ()
+@property(nonatomic, weak) UICollectionView *collectionView;
+@property(nonatomic, strong) NSMutableArray *objectChanges;
+@property(nonatomic, strong) NSMutableArray *sectionChanges;
+@end
+
+@implementation MDMFetchedResultsCollectionViewDataSource
+
+- (id)initWithCollectionView:(UICollectionView *)collectionView
+    fetchedResultsController:(NSFetchedResultsController *)fetchedResultsController {
+
+    if([super init]) {
+
+        _collectionView = collectionView;
+        _fetchedResultsController = fetchedResultsController;
+        [self setupFetchedResultsController:fetchedResultsController];
+    }
+
+    return self;
+}
+
+#pragma mark - Private Methods
+
+- (void)setupFetchedResultsController:(NSFetchedResultsController *)fetchedResultsController {
+
+    fetchedResultsController.delegate = self;
+    BOOL fetchSuccess = [fetchedResultsController performFetch:NULL];
+    ZAssert(fetchSuccess, @"Fetch request does not include sort descriptor that uses the section name.");
+    [self.collectionView reloadData];
+}
+
+- (id)itemAtIndexPath:(NSIndexPath *)path {
+
+    return [self.fetchedResultsController objectAtIndexPath:path];
+}
+
+#pragma mark - Public Setters
+
+- (void)setFetchedResultsController:(NSFetchedResultsController *)fetchedResultsController {
+
+    if(_fetchedResultsController != fetchedResultsController) {
+
+        _fetchedResultsController = fetchedResultsController;
+        [self setupFetchedResultsController:fetchedResultsController];
+    }
+}
+
+- (void)setPaused:(BOOL)paused {
+
+    _paused = paused;
+    if(paused) {
+        self.fetchedResultsController.delegate = nil;
+    } else {
+        self.fetchedResultsController.delegate = self;
+        [self.fetchedResultsController performFetch:NULL];
+        [self.collectionView reloadData];
+    }
+}
+
+#pragma mark - Public Methods
+
+- (id)selectedItem {
+
+    NSIndexPath *path = [self.collectionView indexPathsForSelectedItems].firstObject;
+
+    return path ? [self itemAtIndexPath:path] : nil;
+}
+
+- (NSUInteger)numberOfRowsInSection:(NSUInteger)section {
+
+    if(section < [self.fetchedResultsController.sections count]) {
+
+        return [self.fetchedResultsController.sections[section] numberOfObjects];
+    }
+
+    return 0; // If section doesn't exist return 0
+}
+
+- (NSUInteger)numberOfRowsInAllSections {
+
+    NSUInteger totalRows = 0;
+    NSUInteger totalSections = [self.fetchedResultsController.sections count];
+
+    for(NSUInteger section = 0; section < totalSections; section++) {
+        totalRows = totalRows + [self numberOfRowsInSection:section];
+    }
+
+    return totalRows;
+}
+
+- (NSIndexPath *)indexPathForObject:(id)object {
+
+    return [self.fetchedResultsController indexPathForObject:object];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+
+    return self.fetchedResultsController.sections.count;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+
+    return [self numberOfRowsInSection:(NSUInteger)section];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+
+    id cell = [collectionView dequeueReusableCellWithReuseIdentifier:self.reuseIdentifier forIndexPath:indexPath];
+    id object = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    [self.delegate dataSource:self configureCell:cell withObject:object];
+
+    return cell;
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+
+    NSMutableDictionary *change = [NSMutableDictionary new];
+
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            change[@(type)] = @(sectionIndex);
+            break;
+        case NSFetchedResultsChangeDelete:
+            change[@(type)] = @(sectionIndex);
+            break;
+        case NSFetchedResultsChangeMove:
+            break;
+        case NSFetchedResultsChangeUpdate:
+            NSLog(@"update");
+            break;
+    }
+
+    [self.sectionChanges addObject:change];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+
+    NSMutableDictionary *change = [NSMutableDictionary new];
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            change[@(type)] = newIndexPath;
+            break;
+        case NSFetchedResultsChangeDelete:
+            change[@(type)] = indexPath;
+            break;
+        case NSFetchedResultsChangeUpdate:
+            change[@(type)] = indexPath;
+            break;
+        case NSFetchedResultsChangeMove:
+            change[@(type)] = @[indexPath, newIndexPath];
+            break;
+    }
+    [self.objectChanges addObject:change];
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    if(self.sectionChanges.count > 0) {
+        [self.collectionView performBatchUpdates:^{
+
+            for(NSDictionary *change in self.sectionChanges) {
+                [change enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, id obj, BOOL *stop) {
+
+                    NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                    switch(type) {
+                        case NSFetchedResultsChangeInsert:
+                            [self.collectionView insertSections:[NSIndexSet indexSetWithIndex:[obj unsignedIntegerValue]]];
+                            break;
+                        case NSFetchedResultsChangeDelete:
+                            [self.collectionView deleteSections:[NSIndexSet indexSetWithIndex:[obj unsignedIntegerValue]]];
+                            break;
+                        case NSFetchedResultsChangeUpdate:
+                            [self.collectionView reloadSections:[NSIndexSet indexSetWithIndex:[obj unsignedIntegerValue]]];
+                            break;
+                        case NSFetchedResultsChangeMove:
+                            break;
+                    }
+                }];
+            }
+        }                             completion:nil];
+    }
+
+    if(self.objectChanges.count > 0 && self.sectionChanges.count == 0) {
+
+        if([self shouldReloadCollectionViewToPreventKnownIssue] || self.collectionView.window == nil) {
+            // This is to prevent a bug in UICollectionView from occurring.
+            // The bug presents itself when inserting the first object or deleting the last object in a collection view.
+            // http://stackoverflow.com/questions/12611292/uicollectionview-assertion-failure
+            // This code should be removed once the bug has been fixed, it is tracked in OpenRadar
+            // http://openradar.appspot.com/12954582
+            [self.collectionView reloadData];
+
+        } else {
+
+            [self.collectionView performBatchUpdates:^{
+
+                for(NSDictionary *change in self.objectChanges) {
+                    [change enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, id obj, BOOL *stop) {
+
+                        NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                        switch(type) {
+                            case NSFetchedResultsChangeInsert:
+                                [self.collectionView insertItemsAtIndexPaths:@[obj]];
+                                break;
+                            case NSFetchedResultsChangeDelete:
+                                [self.collectionView deleteItemsAtIndexPaths:@[obj]];
+                                break;
+                            case NSFetchedResultsChangeUpdate:
+                                [self.collectionView reloadItemsAtIndexPaths:@[obj]];
+                                break;
+                            case NSFetchedResultsChangeMove:
+                                [self.collectionView moveItemAtIndexPath:obj[0] toIndexPath:obj[1]];
+                                break;
+                        }
+                    }];
+                }
+            }                             completion:nil];
+        }
+    }
+
+    [self.sectionChanges removeAllObjects];
+    [self.objectChanges removeAllObjects];
+}
+
+- (BOOL)shouldReloadCollectionViewToPreventKnownIssue {
+    __block BOOL shouldReload = NO;
+    for(NSDictionary *change in self.objectChanges) {
+        [change enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+            NSIndexPath *indexPath = obj;
+            switch(type) {
+                case NSFetchedResultsChangeInsert:
+                    shouldReload = [self.collectionView numberOfItemsInSection:indexPath.section] == 0;
+                    break;
+                case NSFetchedResultsChangeDelete:
+                    shouldReload = [self.collectionView numberOfItemsInSection:indexPath.section] == 1;
+                    break;
+                case NSFetchedResultsChangeUpdate:
+                    shouldReload = NO;
+                    break;
+                case NSFetchedResultsChangeMove:
+                    shouldReload = NO;
+                    break;
+            }
+        }];
+    }
+
+    return shouldReload;
+}
+@end
