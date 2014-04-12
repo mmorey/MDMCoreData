67e8d5d0e6c0730623247a14ed22492dce329c0c
Matthew Morey <me@matthewmorey.com>
2014-04-12 15:32:08 -0700

    Fixed init bug.

--- Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
+++ Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
@@ -37,7 +37,8 @@
 - (id)initWithTableView:(UITableView *)tableView
 fetchedResultsController:(NSFetchedResultsController *)fetchedResultsController {
 
-    if ([super init]) {
+    self = [super init];
+    if (self) {
         
         _tableView = tableView;
         _fetchedResultsController = fetchedResultsController;
